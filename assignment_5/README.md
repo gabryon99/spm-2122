b4e60d1de2344ad7d10f1a77d9cfd5d492e7f1ff
Gabriele Pappalardo <leleattolo99@gmail.com>
2022-04-22 18:13:00 +0200

    add assignment 5

---
+++ assignment_5/README.md
@@ -0,0 +1,3 @@
+## Assignment 5
+
+Implementation of a farm using OpenMP. Tasks to be computed have to be provided through a for loop with iterations producing one of the input tasks and then awaiting for Ta time (parameter) before executing next iteration. The parallelism degree of the farm should be a parameter. Each task should spent some parametric amount of time (possibly using the active_wait functioN) to produce the final result.
--- assignment_5/src/main.cpp
+++ assignment_5/src/main.cpp
@@ -4,27 +4,49 @@
 
 #include <omp.h>
 
-/**
- * Implementation of a farm using OpenMP. Tasks to be computed have to be provided through a for loop with iterations 
- * producing one of the input tasks and then awaiting for Ta time (parameter) before executing next iteration. 
- * The parallelism degree of the farm should be a parameter. Each task should spent some parametric amount of 
- * time (possibly using the active_wait function) to produce the final result.
- */
+void execute_farm(std::size_t par_degree, std::size_t tasks, const std::chrono::nanoseconds& ta,  const std::chrono::nanoseconds& ts) {
 
-int main(int argc, char** argv, char** envs) {
+    #pragma omp parallel num_threads(par_degree)
+    {
+        #pragma omp single
+        {
+            // Emit new tasks
+            for (std::size_t i = 0; i < tasks; i++) {
+
+                #pragma omp task
+                {
+                    #pragma omp critical
+                    {
+                        std::cout << "Thread " << omp_get_thread_num() << ": executing task " << i << "...\n";
+                    }
+                    spm::active_delay(ts); // Fake the computation of a task
+                    #pragma omp critical
+                    {
+                        std::cout << "Thread " << omp_get_thread_num() << ": task " << i << " done!\n";
+                    }
+                }
+
+                // Wait to emit a new task
+                std::this_thread::sleep_for(ta); // Emit a new task half a second
+            }
+            #pragma omp taskwait
+            std::cout << "All tasks were completed!\n";
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+
+    using namespace std::chrono_literals;
 
     // Prints out: OMP_NUM_THREADS
     if (const char* env_omp = std::getenv("OMP_NUM_THREADS"))
         std::cout << "OMP_NUM_THREADS: " << env_omp << '\n';
 
     auto workers = omp_get_max_threads();
-    std::cout << "The current workstation can use up to: " << workers << " threads!\n";
+    std::cout << "The current node can use up to " << workers << " threads!\n";
 
-    #pragma omp parallel
-    {
-        #pragma omp critical
-        std::cout << "Hello World from thread " << omp_get_thread_num() << "!\n";
-    }
-  
+    execute_farm(workers, 16, 250ms, 10s);
+
     return EXIT_SUCCESS;
 }
